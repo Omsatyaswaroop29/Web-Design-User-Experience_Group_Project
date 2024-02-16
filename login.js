ec12d2e71deba45efd7d72c9a4a9d6a90de2dab8
gauthamkris7neu <venkatakrishnapras.g@northeastern.edu>
2024-02-16 02:07:27 -0500

    [Grp Project]Login Page Intial Commit

---
+++ login.js
@@ -0,0 +1,6 @@
+function toggleLoginForm() {
+    var x = document.getElementById("loginForm");
+    var y = document.getElementById("initial-butttons");
+        x.style.display = "block";
+        y.style.display = "none";
+}
